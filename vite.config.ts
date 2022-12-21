865d93aa989e2e69a2d9ffa5f64c25909cd4c0d3
Darshan Patel <44410769+imdp7@users.noreply.github.com>
2022-12-21 10:38:36 -0500

    build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis', //<-- AWS SDK
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser', // <-- Fix from above
+    },
+  },
 });
