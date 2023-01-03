0eadadbb82b6bc811d7d6d68bad3f74992c6bf5c
Darshan Patel <44410769+imdp7@users.noreply.github.com>
2023-01-03 08:30:41 -0500

    preferences modal v1

--- src/features/common/TopNavigations.tsx
+++ src/features/common/TopNavigations.tsx
@@ -6,6 +6,8 @@ import {
   Input,
   Box,
   Toggle,
+  FormField,
+  ColumnLayout,
 } from '@cloudscape-design/components';
 import '../../App.css';
 import classes from '../../app.module.scss';
@@ -16,8 +18,10 @@ import {
   applyDensity,
   disableMotion,
 } from '@awsui/global-styles';
-import { SpaceBetween } from '@cloudscape-design/components';
+import { SpaceBetween, Modal, Button } from '@cloudscape-design/components';
 import { useNavigate } from 'react-router-dom';
+import PageNotFound from '../../PageNotFound';
+import { Container } from '@cloudscape-design/components';
 
 interface State {
   user: string;
@@ -27,198 +31,244 @@ interface State {
 export const AppHeader = (props: State): JSX.Element => {
   const [mode, setMode] = useState(false);
   const [density, setDensity] = useState(true);
+  const [motion, setMotion] = useState(true);
   const [searchValue, setSearchValue] = useState('');
   const [redirectURL, setRedirectURL] = useState('');
+  const [visible, setVisible] = React.useState(false);
+
   const navigate = useNavigate();
 
   if (redirectURL == 'signout') {
     navigate('/');
     props.signOut();
-  }
-  if (redirectURL == 'profile') {
+  } else if (redirectURL == 'profile') {
     navigate('/account/profile');
   }
+
+  const onFollowHandler = (e) => {
+    e.preventDefault();
+    e.detail.id === 'preferences' ? setVisible(true) : null;
+  };
+  const onDismissHandler = () => {
+    setVisible(false);
+  };
+  const onSubmit = (value) => {
+    setVisible(false);
+  };
   return (
-    <div id="h" style={{ position: 'sticky', top: 0, zIndex: 1002 }}>
-      <Box float="right" padding={{ right: 'xl', top: 'm' }} textAlign="right">
-        <SpaceBetween size="s" direction="horizontal">
-          <Toggle
-            onChange={({ detail }) => {
-              setMode(detail.checked);
-              applyMode(detail.checked ? Mode.Dark : Mode.Light);
-            }}
-            checked={mode}
-            className={classes.app_header_footer}
-          >
-            {mode ? 'Dark' : 'Light'}
-          </Toggle>
-          <Toggle
-            onChange={({ detail }) => {
-              setDensity(detail.checked);
-              applyDensity(
-                detail.checked ? Density.Comfortable : Density.Compact
-              );
-              disableMotion(density ? true : false);
-            }}
-            checked={density}
-            className={classes.app_header_footer}
-          >
-            {density ? 'Comfort' : 'Compact'}
-          </Toggle>
-        </SpaceBetween>
-      </Box>
-      <TopNavigation
-        identity={{
-          href: '/',
-          logo: {
-            src: 'https://www.tamr.com/wp-content/uploads/2020/07/Logo-AWS.png',
-            alt: 'Service',
-          },
-        }}
-        utilities={[
-          {
-            type: 'button',
-            text: 'Link',
-            href: 'https://example.com/',
-            external: true,
-            externalIconAriaLabel: ' (opens in a new tab)',
-          },
-          {
-            type: 'button',
-            iconName: 'notification',
-            title: 'Notifications',
-            ariaLabel: 'Notifications (unread)',
-            badge: true,
-            disableUtilityCollapse: false,
-          },
-          {
-            type: 'menu-dropdown',
-            iconName: 'settings',
-            ariaLabel: 'Settings',
-            title: 'Settings',
-            items: [
-              {
-                id: 'settings-org',
-                text: 'Organizational settings',
-              },
-              {
-                id: 'settings-project',
-                text: 'Project settings',
-              },
-            ],
-          },
-          {
-            type: 'menu-dropdown',
-            iconName: 'user-profile',
-            ariaLabel: 'Available Zones',
-            text: 'N. Virginia',
-            title: 'Zones',
-            items: [
-              {
-                id: 'us-east-1',
-                text: 'US East (N. Virginia)',
-              },
-              {
-                id: 'us-east-2',
-                text: 'US East Ohio)',
-              },
-              {
-                id: 'us-west-1',
-                text: 'US West (N. California)',
-              },
-              {
-                id: 'us-west-2',
-                text: 'US West (Oregon)',
-              },
-              {
-                id: 'us-East-1',
-                text: 'US East (N. Virginia)',
-              },
-              {
-                id: 'us-East-1',
-                text: 'US East (N. Virginia)',
-              },
-              {
-                id: 'us-East-1',
-                text: 'US East (N. Virginia)',
-              },
-              {
-                id: 'us-East-1',
-                text: 'US East (N. Virginia)',
-              },
-              {
-                id: 'us-East-1',
-                text: 'US East (N. Virginia)',
-              },
-              {
-                id: 'settings-project',
-                text: 'Project settings',
-              },
-            ],
-          },
-          {
-            type: 'menu-dropdown',
-            text: `${props.user}`,
-            // description: 'Account ID: 6137-4001-7922',
-            iconName: 'user-profile',
-            onItemClick: (evt) => {
-              setRedirectURL(evt.detail.id);
+    <>
+      <div id="h" style={{ position: 'sticky', top: 0, zIndex: 1002 }}>
+        <Modal
+          onDismiss={() => setVisible(false)}
+          visible={visible}
+          closeAriaLabel="Close modal"
+          header="Preferences"
+          size="medium"
+        >
+          <SpaceBetween size="s" direction="vertical">
+            <ColumnLayout columns={3}>
+              <FormField description="Light or Dark." label="Theme Mode">
+                <Toggle
+                  onChange={({ detail }) => {
+                    setMode(detail.checked);
+                    applyMode(detail.checked ? Mode.Dark : Mode.Light);
+                  }}
+                  checked={mode}
+                  className={classes.app_header_footer}
+                >
+                  <Box>{mode ? 'Dark' : 'Light'}</Box>
+                </Toggle>
+              </FormField>
+              <FormField description="Comfort or Compact." label="Density">
+                <Toggle
+                  onChange={({ detail }) => {
+                    setDensity(detail.checked);
+                    applyDensity(
+                      detail.checked ? Density.Comfortable : Density.Compact
+                    );
+                    disableMotion(density ? true : false);
+                  }}
+                  checked={density}
+                  className={classes.app_header_footer}
+                >
+                  <Box>{density ? 'Comfort' : 'Compact'}</Box>
+                </Toggle>
+              </FormField>
+              <FormField description="True or False." label="Motion">
+                <Toggle
+                  onChange={({ detail }) => {
+                    setMotion(detail.checked);
+                    disableMotion(motion ? true : false);
+                  }}
+                  checked={motion}
+                  className={classes.app_header_footer}
+                >
+                  <Box>{motion ? 'True' : 'False'}</Box>
+                </Toggle>
+              </FormField>
+            </ColumnLayout>
+          </SpaceBetween>
+        </Modal>
+        <TopNavigation
+          identity={{
+            href: '/',
+            logo: {
+              src: 'https://www.tamr.com/wp-content/uploads/2020/07/Logo-AWS.png',
+              alt: 'Service',
             },
-            items: [
-              {
-                id: 'account',
-                text: 'Account ID: 6107-4191-7922',
-                disabled: true,
+          }}
+          utilities={[
+            {
+              type: 'button',
+              text: 'Link',
+              href: 'https://example.com/',
+              external: true,
+              externalIconAriaLabel: ' (opens in a new tab)',
+            },
+            {
+              type: 'button',
+              iconName: 'notification',
+              title: 'Notifications',
+              ariaLabel: 'Notifications (unread)',
+              badge: true,
+              disableUtilityCollapse: false,
+            },
+            {
+              type: 'menu-dropdown',
+              iconName: 'settings',
+              ariaLabel: 'Settings',
+              title: 'Settings',
+              onItemClick: (evt) => {
+                onFollowHandler(evt);
               },
-              { id: 'profile', text: 'Profile' },
-              { id: 'preferences', text: 'Preferences' },
-              { id: 'security', text: 'Security' },
-              {
-                id: 'support-group',
-                text: 'Support',
-                items: [
-                  {
-                    id: 'documentation',
-                    text: 'Documentation',
-                    href: '#',
-                    external: true,
-                    externalIconAriaLabel: ' (opens in new tab)',
-                  },
-                  { id: 'support', text: 'Support' },
-                  {
-                    id: 'feedback',
-                    text: 'Feedback',
-                    href: '#',
-                    external: true,
-                    externalIconAriaLabel: ' (opens in new tab)',
-                  },
-                ],
+              items: [
+                {
+                  id: 'settings-org',
+                  text: 'Organizational settings',
+                },
+                {
+                  id: 'settings-project',
+                  text: 'Project settings',
+                },
+                {
+                  id: 'preferences',
+                  text: 'Preferences',
+                },
+              ],
+            },
+            {
+              type: 'menu-dropdown',
+              iconName: 'user-profile',
+              ariaLabel: 'Available Zones',
+              text: 'N. Virginia',
+              title: 'Zones',
+              items: [
+                {
+                  id: 'us-east-1',
+                  text: 'US East (N. Virginia)',
+                },
+                {
+                  id: 'us-east-2',
+                  text: 'US East Ohio)',
+                },
+                {
+                  id: 'us-west-1',
+                  text: 'US West (N. California)',
+                },
+                {
+                  id: 'us-west-2',
+                  text: 'US West (Oregon)',
+                },
+                {
+                  id: 'us-East-1',
+                  text: 'US East (N. Virginia)',
+                },
+                {
+                  id: 'us-East-1',
+                  text: 'US East (N. Virginia)',
+                },
+                {
+                  id: 'us-East-1',
+                  text: 'US East (N. Virginia)',
+                },
+                {
+                  id: 'us-East-1',
+                  text: 'US East (N. Virginia)',
+                },
+                {
+                  id: 'us-East-1',
+                  text: 'US East (N. Virginia)',
+                },
+                {
+                  id: 'settings-project',
+                  text: 'Project settings',
+                },
+              ],
+            },
+            {
+              type: 'menu-dropdown',
+              text: `${props.user}`,
+              // description: 'Account ID: 6137-4001-7922',
+              iconName: 'user-profile',
+              onItemClick: (evt) => {
+                setRedirectURL(evt.detail.id);
               },
-              {
-                id: 'signout',
-                text: 'Sign out',
-              },
-            ],
-          },
-        ]}
-        i18nStrings={{
-          searchIconAriaLabel: 'Search',
-          searchDismissIconAriaLabel: 'Close search',
-          overflowMenuTriggerText: 'More',
-          overflowMenuTitleText: 'All',
-          overflowMenuBackIconAriaLabel: 'Back',
-          overflowMenuDismissIconAriaLabel: 'Close menu',
-        }}
-        search={
-          <Input
-            ariaLabel="Input field"
-            value={searchValue}
-            type="search"
-            placeholder="Search"
-            onChange={({ detail }) => setSearchValue(detail.value)}
-          />
-        }
-      />
-    </div>
+              items: [
+                {
+                  id: 'account',
+                  text: 'Account ID: 6107-4191-7922',
+                  disabled: true,
+                },
+                { id: 'profile', text: 'Profile' },
+                { id: 'preferences', text: 'Preferences' },
+                { id: 'security', text: 'Security' },
+                {
+                  id: 'support-group',
+                  text: 'Support',
+                  items: [
+                    {
+                      id: 'documentation',
+                      text: 'Documentation',
+                      href: '#',
+                      external: true,
+                      externalIconAriaLabel: ' (opens in new tab)',
+                    },
+                    { id: 'support', text: 'Support' },
+                    {
+                      id: 'feedback',
+                      text: 'Feedback',
+                      href: '#',
+                      external: true,
+                      externalIconAriaLabel: ' (opens in new tab)',
+                    },
+                  ],
+                },
+                {
+                  id: 'signout',
+                  text: 'Sign out',
+                },
+              ],
+            },
+          ]}
+          i18nStrings={{
+            searchIconAriaLabel: 'Search',
+            searchDismissIconAriaLabel: 'Close search',
+            overflowMenuTriggerText: 'More',
+            overflowMenuTitleText: 'All',
+            overflowMenuBackIconAriaLabel: 'Back',
+            overflowMenuDismissIconAriaLabel: 'Close menu',
+          }}
+          search={
+            <Input
+              ariaLabel="Input field"
+              value={searchValue}
+              type="search"
+              placeholder="Search"
+              onChange={({ detail }) => setSearchValue(detail.value)}
+            />
+          }
+        />
+      </div>
+    </>
   );
 };
